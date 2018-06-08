336369339c6bd96ed59e822ffce2341bfaa538e0
joseant1234 <j-osea-nt@hotmail.com>
2018-06-08 16:53:20 -0500

    add who_we_are, featured_celebrities  section

--- app/controllers/front/home_controller.rb
+++ app/controllers/front/home_controller.rb
@@ -2,7 +2,8 @@ class Front::HomeController < FrontController
 
   def index
     @banner_photos = current_site.photographs.banner.limit(3)
-    @featured_celebrities = Celebrity.match_with_categories_of_site(current_site).active.filter_by_featured.lasted.limit(20)
+    @who_we_are_photos = current_site.photographs.who_we_are.limit(3)
+    @featured_celebrities = Celebrity.match_with_categories_of_site(current_site).active.filter_by_featured.lasted.limit(3)
   end
 
 end
